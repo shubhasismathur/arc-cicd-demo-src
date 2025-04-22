2126474a5157b88861ffeb9e0a69755b17792c7e
Shubhasis Mathur <shmat@microsoft.com>
2025-04-22 19:26:11 +0530

    Removed the helm templates push to the gh repo

--- utils/create-pr.sh
+++ utils/create-pr.sh
@@ -62,6 +62,13 @@ git checkout -b "$deploy_branch_name"
 echo "Copying manifests from $SOURCE_FOLDER to $DEST_FOLDER"
 mkdir -p "$DEST_FOLDER"
 cp -r "$SOURCE_FOLDER"/* "$DEST_FOLDER/"
+
+# Comment out the following lines to exclude templates/dev/azure-vote
+# if [ -d "$DEST_FOLDER/templates/dev/azure-vote" ]; then
+#     echo "Excluding $DEST_FOLDER/templates/dev/azure-vote from commit"
+#     rm -rf "$DEST_FOLDER/templates/dev/azure-vote"
+# fi
+
 git add -A
 git status
 
